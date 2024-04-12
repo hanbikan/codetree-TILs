7bb90ffe2276bdc8575ad8c59d3cabc27e13e1e4
hanbikan <fchopinof99@gmail.com>
2024-04-12 18:02:32 +0900

    Title: 루돌프의 반란, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240412/루돌프의 반란/rudolph-rebellion.py
+++ 240412/루돌프의 반란/rudolph-rebellion.py
@@ -11,6 +11,7 @@ def get_nearest_santa():
     min_distance = float('inf')
     min_p = 0
     for p, (sx, sy) in s_pos.items():
+        if sx == -1: continue
         dist = pow(sx - rx, 2) + pow(sy - ry, 2)
         if dist < min_distance:
             min_distance = dist
@@ -20,6 +21,38 @@ def get_nearest_santa():
                 min_p = p
 
     return min_p
+
+def handle_collision(x, y, nx, ny):
+    if x == nx and y == ny: return
+    dx, dy = int((nx - x) / abs(nx - x)) if nx != x else 0, int((ny - y) / abs(ny - y)) if ny != y else 0
+    to_move = []
+    offset = 0
+
+    while True:
+        cx, cy = nx + dx * offset, ny + dy * offset
+        if not in_range(cx, cy):
+            break
+        if mapp[cx][cy] >= 1:
+            to_move.append(mapp[cx][cy])
+            offset += 1
+        else:
+            break
+
+    for i in range(len(to_move) - 1, -1, -1):
+        cp = to_move[i]
+        cx, cy = s_pos[cp]
+        ncx, ncy = cx + dx, cy + dy
+        if in_range(ncx, ncy):
+            s_pos[cp] = [ncx, ncy]
+            mapp[ncx][ncy] = mapp[cx][cy]
+
+    p = mapp[x][y]
+    if in_range(nx, ny):
+        s_pos[p] = [nx, ny]
+        mapp[nx][ny] = mapp[x][y]
+    else:
+        s_pos[p] = [-1, -1]
+    mapp[x][y] = EMPTY
 
 def move_rudolf(target_p):
     global rx, ry
@@ -37,47 +70,18 @@ def move_rudolf(target_p):
             min_distance = distance
             min_dx, min_dy = dx, dy
 
-    # 이동
+    # 산타 이동 + 상호작용
     nsx, nsy = tx + min_dx * C, ty + min_dy * C # 산타가 튕겨나간 위치
+    if mapp[rx + min_dx][ry + min_dy] >= 1:
+        handle_collision(rx + min_dx, ry + min_dy, nsx, nsy)
+
+        status[target_p] = 2  # 기절
+        scores[target_p] += C # 점수
 
     # 루돌프 이동
     mapp[rx][ry] = EMPTY
     rx, ry = rx + min_dx, ry + min_dy
-    has_collision = mapp[rx][ry] >= 1
     mapp[rx][ry] = RUDOLF
-
-    # 산타 이동 + 상호작용
-    if has_collision:
-        offset = 0
-        to_move = []
-        while True:
-            cur_x, cur_y = nsx + min_dx * offset, nsy + min_dy * offset
-            if not in_range(cur_x, cur_y):
-                break
-            if mapp[cur_x][cur_y] >= 1: # 산타가 있음
-                to_move.append(mapp[cur_x][cur_y])
-                offset += 1
-            else:
-                break
-        # 뒤에 있는 산타부터 이동
-        for i in range(len(to_move) - 1, -1, -1):
-            p_to_move = to_move[i]
-            px, py = s_pos[p_to_move]
-            pnx, pny = px + min_dx, py + min_dy
-            if not in_range(pnx, pny):
-                s_pos.pop(p_to_move)
-            else:
-                mapp[pnx][pny] = mapp[px][py]
-                s_pos[p_to_move] = [pnx, pny]
-
-        if not in_range(nsx, nsy):
-            s_pos.pop(target_p)
-        else:
-            mapp[nsx][nsy] = target_p
-            s_pos[target_p] = [nsx, nsy]
-
-        status[target_p] = 2  # 기절
-        scores[target_p] += C # 점수
 
 def get_next_santa_pos(sx, sy):
     min_distance = pow(sx - rx, 2) + pow(sy - ry, 2)
@@ -99,9 +103,9 @@ def get_next_santa_pos(sx, sy):
 def move_santas():
     p_to_remove = []
     for p in range(1, P + 1):
-        if not s_pos.get(p):
-            continue
         sx, sy = s_pos[p]
+        if sx == -1:
+            continue
         if status[p] != 0:
             continue
         nsx, nsy = get_next_santa_pos(sx, sy)
@@ -111,39 +115,8 @@ def move_santas():
         rdx, rdy = -dx, -dy
 
         if mapp[nsx][nsy] == RUDOLF:
-            # 산타 밀려남
             csx, csy = sx + rdx * (D - 1), sy + rdy * (D - 1)
-            if not (csx == sx and csy == sy):
-                offset = 0
-                to_move = []
-                while True:
-                    cur_x, cur_y = csx + rdx * offset, csy + rdy * offset
-                    if not in_range(cur_x, cur_y):
-                        break
-                    if mapp[cur_x][cur_y] >= 1:
-                        to_move.append(mapp[cur_x][cur_y])
-                        offset += 1
-                    else:
-                        break
-
-                # 뒤에 있는 산타부터 이동
-                for i in range(len(to_move) - 1, -1, -1):
-                    p_to_move = to_move[i]
-                    px, py = s_pos[p_to_move]
-                    pnx, pny = px + rdx, py + rdy
-                    if not in_range(pnx, pny):
-                        p_to_remove.append(p_to_move)
-                    else:
-                        mapp[pnx][pny] = mapp[px][py]
-                        s_pos[p_to_move] = [pnx, pny]
-
-            if not in_range(csx, csy):
-                mapp[sx][sy] = EMPTY
-                p_to_remove.append(p)
-            else:
-                mapp[sx][sy] = EMPTY
-                mapp[csx][csy] = p
-                s_pos[p] = [csx, csy]
+            handle_collision(sx, sy, csx, csy)
 
             status[p] = 2 # 기절
             scores[p] += D # 점수
@@ -151,9 +124,6 @@ def move_santas():
             mapp[sx][sy] = EMPTY
             mapp[nsx][nsy] = p
             s_pos[p] = [nsx, nsy]
-
-    for p in p_to_remove:
-        s_pos.pop(p)
 
 
 '''
@@ -179,6 +149,24 @@ def move_santas():
 5 7 1 1 1
 1 1
 1 2 2
+
+5 7 3 2 1
+1 1
+1 2 2
+2 3 3
+3 4 4
+
+5 7 3 2 1
+1 1
+1 2 2
+2 3 3
+3 5 5
+
+5 7 3 2 1
+1 1
+1 2 2
+2 3 3
+3 5 5
 '''
 
 def print_status():
@@ -209,6 +197,7 @@ def play():
             break
 
         for p in s_pos.keys():
+            if s_pos[p][0] == -1: continue
             scores[p] += 1 # 살아있는 산타 점수 추가
             status[p] = max(0, status[p] - 1) # 상태 변경
 
