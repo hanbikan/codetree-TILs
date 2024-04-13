ea33517843bb58df618ab215e49c107362c55d23
hanbikan <fchopinof99@gmail.com>
2024-04-13 18:40:27 +0900

    Title: 토끼와 경주, Time: 891ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 240413/토끼와 경주/rabit-and-race.py
+++ 240413/토끼와 경주/rabit-and-race.py
@@ -54,52 +54,24 @@ def get_out_position(x, y, d_index):
             else:
                 return [x, diff % M]
 
-def get_next_positions(rabbit):
+def get_next_rabbits(rabbit):
     result = []
     x, y = rabbit.x, rabbit.y
     d = dist[rabbit.pid]
     for k in range(4):
         dx, dy = d_pos[k]
-        nx, ny = x + dx * d[0], y + dy * d[1]
+        nx, ny = x + dx * (d % (N * 2 - 2)), y + dy * (d % (M * 2 - 2))
         if not in_range(nx, ny):
             nx, ny = get_out_position(nx, ny, k)
-        result.append([nx, ny])
+        result.append(Rabbit(nx, ny, 0, 0))
     return result
 
-def select_position(positions):
-    # 번호 + 열 번호가 큰 칸
-    candidates = []
-    max_sum = -1
-    for x, y in positions:
-        if x + y > max_sum:
-            max_sum = x + y
-            candidates = [[x,y]]
-        elif x + y == max_sum:
-            candidates.append([x,y])
-    if len(candidates) == 1:
-        return candidates[0]
-    
-    # 행 번호가 큰 칸
-    next_candidates = []
-    max_x = -1
-    for x, y in candidates:
-        if x > max_x:
-            max_x = x
-            next_candidates = [[x,y]]
-        elif x == max_x:
-            next_candidates.append([x,y])
-    candidates = next_candidates
-    if len(candidates) == 1:
-        return candidates[0]
-
-    # 열 번호가 큰 칸
-    candidate = 0
-    max_y = -1
-    for x, y in candidates:
-        if y > max_y:
-            max_y = y
-            candidate = [x,y]
-    return candidate
+def select_position(rabbits):
+    result = rabbits[0]
+    for r in rabbits:
+        if compare(r, result):
+            result = r
+    return [result.x, result.y]
 
 def compare(r1, r2):
     if r1.x + r1.y != r2.x + r2.y:
@@ -118,32 +90,25 @@ def play(): # 2000
         # 이동
         rabbit_to_move = heapq.heappop(rabbits)
         pid_to_move = rabbit_to_move.pid
-        heapq.heappush(selected, rabbit_to_move)
-        rabbit_to_move.jump += 1
+        selected.append(rabbit_to_move)
     
-        next_positions = get_next_positions(rabbit_to_move) # 4
-        x, y = select_position(next_positions)
-        rabbit_to_move.x = x
-        rabbit_to_move.y = y
-        heapq.heappush(rabbits, rabbit_to_move)
+        next_rabbits = get_next_rabbits(rabbit_to_move) # 4
+        x, y = select_position(next_rabbits)
+        heapq.heappush(rabbits, Rabbit(x, y, rabbit_to_move.jump + 1, pid_to_move))
 
         # 스코어 올리기
         to_add = x + y + 2
         score_to_add += to_add
         score[pid_to_move] -= to_add
-        #print("position", position)
-        #print("dist", dist)
-        #print("score", score)
-        #print("jump", jump_count)
+
+        #for r in rabbits: print(r.pid, "(", r.x, r.y, ")", "dist:", dist[r.pid], "count:", r.jump, "score:", score[r.pid] + score_to_add)
         #print()
     # 우선순위 높은 토끼 +S(단, K번 반복하는 동안 1번이라도 뽑힌 적 있는 토끼여야 함)
     bonus_rabbit = Rabbit(0, 0, 0, 0)
-    while selected:
-        cur = heapq.heappop(selected)
+    for cur in selected:
         if compare(cur, bonus_rabbit):
             bonus_rabbit = cur
     score[bonus_rabbit.pid] += S
-
 
 dist = {}
 score = {}
@@ -158,7 +123,7 @@ for t in range(1, Q + 1):
         N, M, P = order[1], order[2], order[3]
         for i in range(4, len(order), 2):
             pid, d = order[i], order[i + 1]
-            dist[pid] = [d % (N * 2 - 2), d % (M * 2 - 2)]
+            dist[pid] = d
             score[pid] = 0
             heapq.heappush(rabbits, Rabbit(0, 0, 0, pid))
     elif order[0] == 200:
@@ -168,8 +133,7 @@ for t in range(1, Q + 1):
     elif order[0] == 300:
         # 이동거리 변경
         pid, L = order[1], order[2]
-        dist[pid][0] = (dist[pid][0] * L) % (N * 2 - 2)
-        dist[pid][1] = (dist[pid][1] * L) % (M * 2 - 2)
+        dist[pid] *= L
     else:
         # 최고의 토끼 선정
         print(max(score.values()) + score_to_add)
