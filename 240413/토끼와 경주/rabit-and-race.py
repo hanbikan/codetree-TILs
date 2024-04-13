27b80f2197cbd372d04891480cc46a9798633211
hanbikan <fchopinof99@gmail.com>
2024-04-13 17:55:57 +0900

    Title: 토끼와 경주, Time: 9590ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240413/토끼와 경주/rabit-and-race.py
+++ 240413/토끼와 경주/rabit-and-race.py
@@ -4,58 +4,42 @@ d_pos = [
 
 def select_to_move():
     # 현재까지의 총 점프 횟수가 적은 토끼
-    candidates = []
+    candidate = 0
     min_jump_count = float('inf')
-    for pid, count in jump_count.items():
-        if count < min_jump_count:
-            min_jump_count = count
-            candidates = [pid]
-        elif count == min_jump_count:
-            candidates.append(pid)
-    if len(candidates) == 1:
-        return candidates[0]
-    
-    # 현재 서있는 행 번호 + 열 번호가 작은 토끼
-    next_candidates = []
     min_sum = float('inf')
-    for pid in candidates:
-        x, y = position[pid]
-        if x + y < min_sum:
-            min_sum = x + y
-            next_candidates = [pid]
-        elif x + y == min_sum:
-            next_candidates.append(pid)
-    candidates = next_candidates
-    if len(candidates) == 1:
-        return candidates[0]
-    
-    # 행 번호가 작은 토끼
-    next_candidates = []
     min_x = float('inf')
-    for pid in candidates:
-        x, y = position[pid]
-        if x < min_x:
-            min_x = x
-            next_candidates = [pid]
-        elif x == min_x:
-            next_candidates.append(pid)
-    candidates = next_candidates
-    if len(candidates) == 1:
-        return candidates[0]
-    
-    # 열 번호가 작은 토끼
-    next_candidates = []
     min_y = float('inf')
-    for pid in candidates:
+    min_pid = float('inf')
+    for pid, count in jump_count.items():
         x, y = position[pid]
-        if y < min_y:
-            min_y = y
-            next_candidates = [pid]
-        elif y == min_y:
-            next_candidates.append(pid)
-    candidates = next_candidates
-    if len(candidates) == 1:
-        return candidates[0]
+        if count < min_jump_count:
+            candidate = pid
+            min_jump_count = count
+            min_sum = x + y
+        elif count == min_jump_count:
+            # 현재 서있는 행 번호 + 열 번호가 작은 토끼
+            if x + y < min_sum:
+                candidate = pid
+                min_sum = x + y
+                min_x = x
+            elif x + y == min_sum:
+                # 행 번호가 작은 토끼
+                if x < min_x:
+                    candidate = pid
+                    min_x = x
+                    min_y = y
+                elif x == min_x:
+                    # 열 번호가 작은 토끼
+                    if y < min_y:
+                        candidate = pid
+                        min_y = y
+                        min_pid = pid
+                    elif y == min_y:
+                        if pid < min_pid:
+                            candidate = pid
+                            min_pid = pid
+
+    return candidate
     
     # 고유번호가 작은 토끼
     min_pid = float('inf')
@@ -102,7 +86,7 @@ def get_next_positions(pid):
     d = dist[pid]
     for k in range(4):
         dx, dy = d_pos[k]
-        nx, ny = x + (dx * d) % (N * 2 - 2), y + (dy * d) % (M * 2 - 2)
+        nx, ny = x + dx * d[0], y + dy * d[1]
         if not in_range(nx, ny):
             nx, ny = get_out_position(nx, ny, k)
         result.append([nx, ny])
@@ -192,24 +176,24 @@ def select_to_bonus(selected):
             max_pid = pid
     return max_pid
 
-def play(): # K, S
+def play(): # 2000
+    global score_to_add
     # K번 반복
     selected = set()
-    for _ in range(K):
+    for _ in range(K): # 100
         # 이동
-        pid_to_move = select_to_move()
+        pid_to_move = select_to_move() # 2000
         selected.add(pid_to_move)
         jump_count[pid_to_move] += 1
-        next_positions = get_next_positions(pid_to_move)
+    
+        next_positions = get_next_positions(pid_to_move) # 4
         x, y = select_position(next_positions)
         position[pid_to_move] = [x,y]
 
         # 스코어 올리기
         to_add = x + y + 2
-        for pid in score.keys():
-            if pid == pid_to_move:
-                continue
-            score[pid] += to_add
+        score_to_add += to_add
+        score[pid_to_move] -= to_add
         #print("position", position)
         #print("dist", dist)
         #print("score", score)
@@ -224,27 +208,29 @@ dist = {}
 score = {}
 jump_count = {}
 position = {}
+score_to_add = 0
 
 Q = int(input())
 for t in range(1, Q + 1):
     order = list(map(int,input().split()))
     if order[0] == 100:
         # 경주 시작 준비
-        N, M, P = order[1:4]
+        N, M, P = order[1], order[2], order[3]
         for i in range(4, len(order), 2):
-            pid, d = order[i:i+2]
-            dist[pid] = d
+            pid, d = order[i], order[i + 1]
+            dist[pid] = [d % (N * 2 - 2), d % (M * 2 - 2)]
             score[pid] = 0
             jump_count[pid] = 0
             position[pid] = [0,0]
     elif order[0] == 200:
         # 경주 진행
-        K, S = order[1:3]
+        K, S = order[1], order[2]
         play()
     elif order[0] == 300:
         # 이동거리 변경
-        pid, L = order[1:3]
-        dist[pid] *= L
+        pid, L = order[1], order[2]
+        dist[pid][0] = (dist[pid][0] * L) % (N * 2 - 2)
+        dist[pid][1] = (dist[pid][1] * L) % (M * 2 - 2)
     else:
         # 최고의 토끼 선정
-        print(max(score.values()))
+        print(max(score.values()) + score_to_add)
